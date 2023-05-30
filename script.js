925223e0389bbce3bb54b070cbba47ad31517db9
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-05-30 11:35:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Example
-var ourName = "freeCodeCamp";
-var ourStr = "Hello, our name is" + ourName + ", how are you?";
-
-// Only change code below this line
-var myName = "Roberto";
-var myStr = "My name is " + myName + ", and I am well";
-console.log(myStr)
